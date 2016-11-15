63b36872f959047f5a4000e64a9c311d70ec17d9
Rac9096 <r.cifuentesfaras@uandresbello.edu>
2016-11-15 14:41:11 -0300

    Add files via upload

---
+++ biblios.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+typedef struct
+{
+  int costo;
+  int vehiculo;
+  int periodicidad[5];
+}arista;
+
+
+
+
+typedef vector <int> v_1; // este es un vector que guarda enteros y se declara como v_1
+typedef vector <v_1> matriz; // este es un vector que guarda vectores y se declara como matriz
---
+++ m_costo.cpp
@@ -0,0 +1,29 @@
+#include "biblios.h"
+
+
+
+
+
+
+
+int main()
+{
+  arista ar[5][5];
+
+  for(int i=0; i<5; i++)
+  {
+    for (int j=0; j<5; j++)
+    {
+      ar[i][j].costo = 0;
+      ar[i][j].vehiculo = 0;
+      ar[i][j].periodicidad[0] = 0;
+      cout << ar[i][j].costo << "-" << ar[i][j].vehiculo << "-" << ar[i][j].periodicidad[0] << "\t";
+    }
+    cout << endl;
+
+  }
+    v_1 v_chico;
+    matriz v_grande;
+
+
+}
